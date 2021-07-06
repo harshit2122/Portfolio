1c731435b9673f7d870ba35190eee223bbde9457
Harshit Joshi <500068141@stu.upes.ac.in>
2021-07-06 14:58:37 +0530

    Project and Experience added

--- src/components/experiencesection/experience.js
+++ src/components/experiencesection/experience.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import './experience.css'
 
 function experience() {
     return (
-        <div>
-            <h1>Hey</h1>
+        <div class="experience">
+            <div className="box">
+                <h3 className="title"> Company Name</h3>
+                <p className="text"> Lorem ipsum dolor sit amet, qui fabellas disputando te. Eam hinc eripuit in, in per cibo autem epicurei, movet mentitum efficiendi his ex.</p>
+                <p className="text1"> Languages Used</p>
+                <p className="text1"> Company Link </p> 
+            </div>
+            <div className="box1">
+                <h3 className="title"> Company Name</h3>
+                <p className="text"> Lorem ipsum dolor sit amet, qui fabellas disputando te. Eam hinc eripuit in, in per cibo autem epicurei, movet mentitum efficiendi his ex.</p>
+                <p className="text1"> Languages Used</p>
+                <p className="text1"> Company Link </p>
+            </div>
+            <div className="box">
+                <h3 className="title"> Company Name</h3>
+                <p className="text"> Lorem ipsum dolor sit amet, qui fabellas disputando te. Eam hinc eripuit in, in per cibo autem epicurei, movet mentitum efficiendi his ex.</p>
+                <p className="text1"> Languages Used</p>
+                <p className="text1"> Company Link </p>
+            </div>
+            <div className="box1">
+                <h3 className="title"> Company Name</h3>
+                <p className="text"> Lorem ipsum dolor sit amet, qui fabellas disputando te. Eam hinc eripuit in, in per cibo autem epicurei, movet mentitum efficiendi his ex.</p>
+                <p className="text1"> Languages Used</p>
+                <p className="text1"> Company Link </p>
+            </div>
+            <div className="box">
+                <h3 className="title"> Company Name</h3>
+                <p className="text"> Lorem ipsum dolor sit amet, qui fabellas disputando te. Eam hinc eripuit in, in per cibo autem epicurei, movet mentitum efficiendi his ex.</p>
+                <p className="text1"> Languages Used</p>
+                <p className="text1"> Company Link </p>
+            </div>
+            <div className="box1">
+                <h3 className="title"> Company Name</h3>
+                <p className="text"> Lorem ipsum dolor sit amet, qui fabellas disputando te. Eam hinc eripuit in, in per cibo autem epicurei, movet mentitum efficiendi his ex.</p>
+                <p className="text1"> Languages Used</p>
+                <p className="text1"> Company Link </p>
+            </div>
         </div>
     )
 }
--- src/components/projectsection/project.js
+++ src/components/projectsection/project.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import './project.css'
 
 function project() {
     return (
-        <div>
-            <h1>Hello</h1>
+        <div className="project">
+            <div className="box">
+                <h3 className="title"> Project Title</h3>
+                <p className="text"> Lorem ipsum dolor sit amet, qui fabellas disputando te. Eam hinc eripuit in, in per cibo autem epicurei, movet mentitum efficiendi his ex.</p>
+                <p className="text1"> Languages Used</p>
+                <p className="text1"> Link of Project</p> 
+            </div>
+            <div className="box1">
+            <h3 className="title"> Project Title</h3>
+                <p className="text"> Lorem ipsum dolor sit amet, qui fabellas disputando te. Eam hinc eripuit in, in per cibo autem epicurei, movet mentitum efficiendi his ex.</p>
+                <p className="text1"> Languages Used</p>
+                <p className="text1"> Link of Project</p>
+            </div>
+            <div className="box">
+            <h3 className="title"> Project Title</h3>
+                <p className="text"> Lorem ipsum dolor sit amet, qui fabellas disputando te. Eam hinc eripuit in, in per cibo autem epicurei, movet mentitum efficiendi his ex.</p>
+                <p className="text1"> Languages Used</p>
+                <p className="text1"> Link of Project</p>
+            </div>
+            <div className="box1">
+            <h3 className="title"> Project Title</h3>
+                <p className="text"> Lorem ipsum dolor sit amet, qui fabellas disputando te. Eam hinc eripuit in, in per cibo autem epicurei, movet mentitum efficiendi his ex.</p>
+                <p className="text1"> Languages Used</p>
+                <p className="text1"> Link of Project</p>
+            </div>
+            <div className="box">
+                <h3 className="title"> Project Title</h3>
+                <p className="text"> Lorem ipsum dolor sit amet, qui fabellas disputando te. Eam hinc eripuit in, in per cibo autem epicurei, movet mentitum efficiendi his ex.</p>
+                <p className="text1"> Languages Used</p>
+                <p className="text1"> Link of Project</p> 
+            </div>
+            <div className="box1">
+            <h3 className="title"> Project Title</h3>
+                <p className="text"> Lorem ipsum dolor sit amet, qui fabellas disputando te. Eam hinc eripuit in, in per cibo autem epicurei, movet mentitum efficiendi his ex.</p>
+                <p className="text1"> Languages Used</p>
+                <p className="text1"> Link of Project</p>
+            </div>
         </div>
     )
 }
